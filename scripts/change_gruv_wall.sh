3a8e86ca7ac7d3f3784ab7f96ce7d05c6bb3dccb
tejes <tmank14319@gmail.com>
2023-08-01 20:00:37 +0530

    theme:rose-pine

--- scripts/change_gruv_wall.sh
+++ scripts/change_gruv_wall.sh
@@ -6,7 +6,7 @@ tran=(
 select_random() {
     printf "%s\0" "$@" | shuf -z -n1 | tr -d '\0'
 }
-swww img "/home/tejes/wallpapers/Gruv/wall_secondary.png"  --transition-bezier .43,1.19,1,.4 \
+swww img "/home/tejes/gruvbox-wall/cPmfgzR.png"  --transition-bezier .43,1.19,1,.4 \
     --transition-type center \
     --transition-duration 1 \
     --transition-fps 144 \
