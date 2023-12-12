a4d91b37c3506ac7a22d335f28410d78caa112db
tejes <tmank14319@gmail.com>
2023-12-13 01:26:43 +0530

    initial commit

--- scripts/change_gruv_wall.sh
+++ scripts/change_gruv_wall.sh
@@ -6,8 +6,4 @@ tran=(
 select_random() {
     printf "%s\0" "$@" | shuf -z -n1 | tr -d '\0'
 }
-swww img "/home/tejes/gruvbox-wall/cPmfgzR.png"  --transition-bezier .43,1.19,1,.4 \
-    --transition-type center \
-    --transition-duration 1 \
-    --transition-fps 144 \
-    --transition-pos top
+swww img "/home/tejes/rose2.jpg"  
